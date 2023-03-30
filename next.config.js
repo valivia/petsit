d5199b4e275d03cf9b292a0de0fe838725312ae2
valivia <valivia@xayania.com>
2023-03-30 18:55:27 +0000

    basic setup

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  poweredByHeader: false,
   experimental: {
     appDir: true,
+    typedRoutes: true,
   },
+  images: {
+    domains: ["authjs.dev"]
+  }
 }
 
 module.exports = nextConfig
