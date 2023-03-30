9049e16887e6a1bc7021990757d422cfd2cbc64d
valivia <valivia@xayania.com>
2023-03-30 19:09:49 +0000

    added editor config

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -2,16 +2,16 @@ import styles from "./layout.module.scss";
 import type { Metadata } from "next/types";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-    return (
-        <>
-            <nav>navbar</nav>
-            {children}
-            <footer>footer</footer>
-        </>
-    );
+  return (
+    <>
+      <nav>navbar</nav>
+      {children}
+      <footer>footer</footer>
+    </>
+  );
 }
 
 export const metadata: Metadata = {
-    title: "petsit",
-    description: "",
+  title: "petsit",
+  description: "",
 };
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -8,22 +8,22 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient()
 
 export const authOptions: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID as string,
-            clientSecret: process.env.GOOGLE_SECRET as string
-        }),
-        SpotifyProvider({
-            clientId: process.env.SPOTIFY_CLIENT_ID as string,
-            clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string
-        }),
-        DiscordProvider({
-            clientId: process.env.DISCORD_CLIENT_ID as string,
-            clientSecret: process.env.DISCORD_CLIENT_SECRET as string
-        })
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string
+    }),
+    SpotifyProvider({
+      clientId: process.env.SPOTIFY_CLIENT_ID as string,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string
+    }),
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID as string,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET as string
+    })
 
-    ]
+  ]
 }
 
 export default NextAuth(authOptions)
