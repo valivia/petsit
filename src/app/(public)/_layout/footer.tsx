0267ab23e5d4b2ced78f37ca5db1e58d40a96ba0
valivia <valivia@xayania.com>
2023-04-03 14:33:52 +0000

    added footer

---
+++ src/app/(public)/_layout/footer.tsx
@@ -0,0 +1,10 @@
+import styles from "./footer.module.scss";
+import Image from "next/image";
+
+export const Footer = () => {
+  return (
+    <footer className={styles.main}>
+      🦶
+    </footer>
+  );
+};
